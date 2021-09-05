f7d06e6c863a49d17832a95d42e21f7e6d5bb37d
anushan1508 <anushan1508@gmail.com>
2021-09-05 20:50:23 +0530

    finish dashboard

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import SightBar from "./component/sight-bar";
+import SalesCardDetails from "./component/sales-card-details";
+import Graph from "./component/graph";
+import Table from "./component/table";
+import DashboardLayout from "./component/dashboard-layout";
+
+function App() {
+  return (
+    <div className="App">
+       <DashboardLayout/>
+
+    </div>
+  );
+}
+
+export default App;
